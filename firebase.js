e992439fc593f82f76c523800f0447d25eadf293
eFoot Game <dicord22samp@gmail.com>
2026-01-23 03:09:29 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
  // ⚠️ عوّض القيم من Firebase Console
   const firebaseConfig = {
-    apiKey: "AIzaSyAM7gLKuLRfhFdWyakFS1jU4c8xU1fg-FU",
-  authDomain: "family-bank-966ae.firebaseapp.com",
-  databaseURL: "https://family-bank-966ae-default-rtdb.firebaseio.com",
-  projectId: "family-bank-966ae",
-  storageBucket: "family-bank-966ae.firebasestorage.app",
-  messagingSenderId: "479496038450",
-  appId: "1:479496038450:web:49ee61a36a621abd3c742b",
+  apiKey: "AIzaSyCHOrJmI1qXr2bbucjq8pXdRCeByPCZqyg",
+  authDomain: "perfect-65fc7.firebaseapp.com",
+  databaseURL: "https://perfect-65fc7-default-rtdb.firebaseio.com",
+  projectId: "perfect-65fc7",
+  storageBucket: "perfect-65fc7.firebasestorage.app",
+  messagingSenderId: "833367031106",
+  appId: "1:833367031106:web:4e2724c393415ecf0e5cdb",
   };
 
   // 🔥 Initialize Firebase
@@ -15,3 +15,4 @@
   // ✅ اختصارات نستعملهم في كل الصفحات
   const auth = firebase.auth();
   const db   = firebase.database();
+
